01f872129f18238a4e3b9d2460c3c3beb149f513
Regis Lima <regislima1391@gmail.com>
2018-04-30 21:04:59 -0300

    Implementado o metodo para mostrar uma conta criada

--- ContaBacariaConsole/Conta.cs
+++ ContaBacariaConsole/Conta.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ContaBacariaConsole
 {
     class Conta
     {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
+
+        public bool HaveraDeposito(char resposta)
+        {
+            if (resposta == 's')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular 
+                + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
     }
 }
--- ContaBacariaConsole/Program.cs
+++ ContaBacariaConsole/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ContaBacariaConsole
 {
     class Program
     {
+        //Programa simples de console para cadastro de uma conta bancária.
+
         static void Main(string[] args)
         {
+            //Cadastro:
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Conta conta = new Conta(numero, titular);
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (conta.HaveraDeposito(resposta))
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular, saldo);
+            }
+
+            //Mostrar conta:
+            Console.WriteLine("Conta criada com sucesso:");
+            Console.WriteLine(conta);
+
+            Console.ReadLine();
         }
     }
 }
